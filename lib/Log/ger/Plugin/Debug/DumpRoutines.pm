70369e1d27424db9d8f3cb447cdf0642b864daa1
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-03-04 09:22:46 +0700

    Initial commit

---
+++ lib/Log/ger/Plugin/Debug/DumpRoutines.pm
@@ -0,0 +1,46 @@
+package Log::ger::Plugin::Debug::DumpRoutines;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use strict;
+use warnings;
+use Data::Dump;
+
+sub get_hooks {
+    my %conf = @_;
+
+    return {
+        before_install_routines => [
+            __PACKAGE__, # key
+            99,          # priority (after all the other plugins)
+            sub {        # hook
+                my %hook_args = @_;
+
+                dump $hook_args{routines};
+                [undef];
+            },
+        ],
+    };
+}
+
+1;
+# ABSTRACT: Dump routines before Log::ger instals them
+
+=for Pod::Coverage ^(.+)$
+
+=head1 SYNOPSIS
+
+ use Log::ger::Plugin->set('Debug::DumpRoutines');
+ use Log::ger;
+
+
+=head1 DESCRIPTION
+
+
+=head1 CONFIGURATION
+
+
+=head1 SEE ALSO
